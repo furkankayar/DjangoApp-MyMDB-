cc4595c39e9091787fdfbbf9156817bda8369889
furkankayar <furkan@localhost.localdomain>
2019-08-31 01:05:43 +0300

    First application core has been created and connection has been established with postgresql.

--- MyMDB/django/config/settings.py
+++ MyMDB/django/config/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mymdb',
+        'USER': 'mymdb',
+        'PASSWORD': 'development',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
---
+++ MyMDB/django/core/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Movie(models.Model):
+    NOT_RATED = 0
+    RATED_G = 1
+    RATED_PG = 2
+    RATED_R = 3
+    RATINGS = (
+                (NOT_RATED, 'NR - Not Rated'),
+                (RATED_G, 'G - General Audiences'),
+                (RATED_PG, 'PG - Parental Guidance Suggested'),
+                (RATED_R, 'R - Restricted'),
+            )
+    """
+    # NOTE: Django will add and istance method to our model called
+    get_rating_display(), which will return the matching second element for the
+    value stored in our model. Anything that doesn't match one of the values in
+    choices will be a ValidationError on save.
+    """
+    title = models.CharField(max_length=140)
+    plot = models.TextField()
+    year = models.PositiveIntegerField()
+    rating = models.IntegerField(choices=RATINGS, default=NOT_RATED)
+    runtime = models.PositiveIntegerField()
+    website = models.URLField(blank = True)
+
+    def __str__(self):
+        return '{} ({})'.format(self.title, self.year)
