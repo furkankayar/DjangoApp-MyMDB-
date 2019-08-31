fc09d524e4b9bcf80dd51a19e36a7c696933f18a
furkankayar <furkan@localhost.localdomain>
2019-09-01 00:23:34 +0300

    Movie detail page has been created by using DetailView class. And base.html template has been created.

--- MyMDB/django/core/urls.py
+++ MyMDB/django/core/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'core'
 urlpatterns = [
     path('movies', views.MovieList.as_view(), name='MovieList'),
+    path('movie/<int:pk>', views.MovieDetail.as_view(), name='MovieDetail'),
+    # NOTE: There must be <int:pk> this is special case for MovieDetail view.
 
 ]
--- MyMDB/django/core/views.py
+++ MyMDB/django/core/views.py
@@ -10,16 +10,26 @@ that you can subclass to easily (almost declaratively) write views to
 accomplish common tasks.
 """
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from core.models import Movie
 
 class MovieList(ListView):
 
     """
     # NOTE: ListView requires at least a model attribute. It will query for all rows
-    of that model, pass it to the template, and return the rendered template in a responseç
+    of that model, pass it to the template, and return the rendered template in a response.
     It also offers a number of hooks that we may use to replace default behavior, which
     are fully documented.
     """
 
+    model = Movie
+
+class MovieDetail(DetailView):
+
+    """
+    # NOTE: A DetailView requires that a path() object include either a pk or slug in
+    the path string so that DetailView can pass that value to the QuerySet to query for
+    a specific model instance. A slug is a short URL-friendly label that is often used
+    in content-heavy sites, as it is SEO friendly.
+    """
     model = Movie
