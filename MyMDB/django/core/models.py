aac8abdf60bc94615627bcf50f624871cac893cc
furkankayar <furkan@localhost.localdomain>
2019-09-01 16:46:34 +0300

    New Person and Role models have been created and relations between movie has been established.

--- MyMDB/django/core/models.py
+++ MyMDB/django/core/models.py
@@ -25,12 +25,64 @@ class Movie(models.Model):
     rating = models.IntegerField(choices=RATINGS, default=NOT_RATED)
     runtime = models.PositiveIntegerField()
     website = models.URLField(blank = True)
-
+    director = models.ForeignKey(
+        to='Person',
+        on_delete=models.SET_NULL,
+        related_name='directed',
+        null=True,
+        blank=True)
+    # NOTE: related_name offers person.directed.add(movie) usage.
+    writers = models.ManyToManyField(
+        to='Person',
+        related_name='writing_credits',
+        blank=True)
+    # NOTE: person.writing_credits.add(movie) same as movie.writers.add(person)
+    actors = models.ManyToManyField(
+        to='Person',
+        through='Role',
+        related_name='acting_credits',
+        blank=True)
+    # NOTE: ManyToManyField with a through class.
 
     class Meta:
         ordering = ('-year', 'title')
         # NOTE: It is like ORDER BY year DESC, title ASC
-        
+
 
     def __str__(self):
         return '{} ({})'.format(self.title, self.year)
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=140)
+    last_name = models.CharField(max_length=140)
+    born = models.DateField()
+    died = models.DateField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('last_name', 'first_name')
+
+    def __str__(self):
+        if self.died:
+            return '{}, {} ({}-{})'.format(
+                self.last_name,
+                self.first_name,
+                self.born,
+                self.died)
+
+        return '{}, {} ({})'.format(
+            self.last_name,
+            self.first_name,
+            self.born)
+
+
+class Role(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.DO_NOTHING)
+    person = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=140)
+
+    def __str__(self):
+        return "{} {} {}".format(self.movie_id, self.person_id, self.name)
+
+    class Meta:
+        ordering = ('movie', 'person', 'name')
